a78e4576225a22a29d4972192d3cd1951bd292d7
yla382 <yoonhlee0831@gmail.com>
2021-11-21 11:49:49 -0800

    components fixed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import CurrentWeatherContainer from './components/CurrentWeatherContainer';
 import WeeklyWeatherContainer from './components/WeeklyWeatherContainer';
 import desert from './img/desert.jpg';
 
-import axios from 'axios';
-
-const options = {
-  method: 'GET',
-  url: 'https://weatherapi.pelmorex.com/api/v1/observation/placecode/cabc0308',
-};
-
 
 function App() {
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
-
   return (
     <div className="App" style={{ backgroundImage: `url(${desert})`, backgroundSize: "cover"}}>
         <CurrentWeatherContainer/>
---
+++ src/util.js
@@ -0,0 +1,18 @@
+export const getDateOfISOWeek = (w, y) => {
+    var simple = new Date(y, 0, 1 + (w - 1) * 7);
+    var dow = simple.getDay();
+    var ISOweekStart = simple;
+    if (dow <= 4)
+        ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+    else
+        ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+    return ISOweekStart;
+};
+
+export const getWeekNumber = (d) => {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    return [d.getUTCFullYear(), weekNo]
+};
